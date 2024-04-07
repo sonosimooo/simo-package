19564b8003b4bae60d9ba5071f3213dc3629d090
Loreeh_ <128195197+Loreehh@users.noreply.github.com>
2024-04-07 10:23:27 +0200

    0.1.6

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
-const { log } = require('simo-package');
-
-log("ciaooooo")
+const { ping } = require('simo-package');
+ping ('google.com')
+  .then(latency => {
+    console.log(`Tempo di risposta per ${host}: ${latency} ms`);
+  })
+  .catch(error => console.error(error.message));
